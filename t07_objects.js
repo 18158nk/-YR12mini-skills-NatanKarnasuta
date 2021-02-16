de28d80762fc7ae8411167fab151bb22a80ef22d
Natan Karnasuta <18158nk@hvhs.school.nz>
2021-02-16 01:35:48 +0000

    to7

--- t07_objects.js
+++ t07_objects.js
@@ -1,31 +1,94 @@
-var x = 25;
-var y = 25;
-var speedY = 2;
-var speedX = 6;
+var ball1 = {
+  posX: 30,
+  posY: 30,
+  directionX: 5,
+  directionY: 6,
+  diameter: 25,
+};
+
+var ball2 = {
+  posX: 100,
+  posY: 100,
+  directionX: -3,
+  directionY: 4,
+  diameter: 40,
+ 
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function setup() {
-  createCanvas(600, 400);
-background (0, 59, 100);
+  createCanvas(400, 400);
+
+
 }
 
 function draw() {
-    background (0, 59, 100);
-  fill(0, 0, 255);
-  ellipse(x, y, 25, 25);
+  background(200, 200);
+
+  // ball1 colour aqua
+  fill(0, 255, 255);
+  ellipse(ball1.posX, ball1.posY, ball1.diameter);
+
+  fill(50, 75, 255)
+  ellipse(ball2.posX, ball2.posY, ball2.diameter);
+
+  //ball 1 positioning/moving code
+  ball1.posY = ball1.posY + ball1.speedY;
+  ball1.posX = ball1.posX + ball1.speedX;
+  //Ball 2positioning/moving code
+  ball2.posY = ball2.posY + ball2.speedY;
+  ball2.posX = ball2.posX + ball2.speedX;
 
 
 
-  if (x > width) {
-    speedX = speedX *-1;
-  } else if (x < 12) {
-    speedX = speedX*-1;
+  if (ball1.posX >= (width - (ball1.diameter / 2))) {
+
+    ball1.speedX = ball1.speedX * -1;
+  } else if (ball1.posX <= ((width - width) + (ball1.diameter / 2))) {
+    ball1.speedX = ball1.speedX * -1;
   }
-  x = x + speedX;
-
-
-  if (y > height) {
-    speedY = speedY *-1;
-  } else if (y < 12) {
-    speedY = speedY *-1;
+  //checks if balls are off screen on X axis
+  if (ball1.posY >= (height - (ball1.diameter / 2))) {
+    ball1.speedY = ball1.speedY * -1;
+  } else if (ball1.posY <= ((height - height) + (ball1.diameter / 2))) {
+    ball1.speedY = ball1.speedY * -1;
   }
- y = y + speedY;
+  if (ball2.posX >= (width - (ball2.diameter / 2))) {
+    ball2.speedX = ball2.speedX * -1;
+  } else if (ball2.posX <= ((width - width) + (ball2.diameter / 2))) {
+    ball2.speedX = ball2.speedX * -1;
+  }
+  if (ball2.posY >= (height - (ball2.diameter / 2))) {
+    ball2.speedY = ball2.speedY * -1;
+  } else if (ball2.posY <= ((height - height) + (ball2.diameter / 2))) {
+    ball2.speedY = ball2.speedY * -1;
+  }
+
 }
+
+
+
+
+
+
