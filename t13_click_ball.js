2321fc2af54ab2150f7c990b15e4752ede806bca
Natan Karnasuta <18158nk@hvhs.school.nz>
2021-03-05 01:37:56 +0000

    Speed Variables

--- t13_click_ball.js
+++ t13_click_ball.js
@@ -3,7 +3,7 @@
 //*****************/
 //*****************/
 var ballsArray = [];
-var speedArray = [10, 9, 8, 7, 6, -6, -7, -8, -9, -10];
+var speedArray = [6, 5, 4, 3, 2, 1, -1, -2, -3, -4, -5, -6];
 const NUMBALLS = [10];
 var score = 0;
 var scoreText;
